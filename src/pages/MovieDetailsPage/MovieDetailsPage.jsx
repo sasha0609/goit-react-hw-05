8b4d5db571b9e02db4dfadd867a9d47dd6ce7892
sasha0609 <oleksandralavr@gmail.com>
2024-06-08 00:03:13 +0200

    update movieDetailsPage and MoviesPage

--- src/pages/MovieDetailsPage/MovieDetailsPage.jsx
+++ src/pages/MovieDetailsPage/MovieDetailsPage.jsx
@@ -1,3 +1,69 @@
+// import {
+//   NavLink,
+//   Link,
+//   Outlet,
+//   useLocation,
+//   useParams,
+// } from "react-router-dom";
+// import { useState, useEffect } from "react";
+// import { fetchMovieDetails } from "../../tmdbApi";
+
+// export default function MoviesDetailsPage() {
+//   const { movieId } = useParams();
+//   const [movie, setMovie] = useState(null);
+//   const [error, setError] = useState(false);
+//   const [loading, setLoading] = useState(false);
+//   const location = useLocation();
+//   useEffect(() => {
+//     const getMovieDetails = async () => {
+//       try {
+//         setError(false);
+//         setLoading(true);
+//         const movieData = await fetchMovieDetails(movieId);
+//         setMovie(movieData);
+//         setLoading(false);
+//       } catch (error) {
+//         console.log("Oops! Please try again!");
+//         setError(true);
+//       } finally {
+//         setLoading(false);
+//       }
+//     };
+
+//     getMovieDetails();
+//   }, [movieId]);
+//   if (!movie) {
+//     return null;
+//   }
+
+//   return (
+//     <div>
+//       {error && <p>Oops! Try again</p>}
+//       {loading && <p>Loading...</p>}
+//       <p>
+//         <Link to={location.state}>Go back</Link>
+//       </p>
+//       <h1>{movie.title}</h1>
+//       <img
+//         src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path}`}
+//         alt={movie.title}
+//       />
+//       <p>{movie.overview}</p>
+//       <p>Release Date: {movie.release_date}</p>
+//       <p>Rating: {movie.vote_average}</p>
+
+//       <ul>
+//         <li>
+//           <NavLink to="cast">Cast</NavLink>
+//         </li>
+//         <li>
+//           <NavLink to="reviews">Reviews</NavLink>
+//         </li>
+//       </ul>
+//       <Outlet />
+//     </div>
+//   );
+// }
 import {
   NavLink,
   Link,
@@ -5,7 +71,7 @@ import {
   useLocation,
   useParams,
 } from "react-router-dom";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { fetchMovieDetails } from "../../tmdbApi";
 
 export default function MoviesDetailsPage() {
@@ -14,6 +80,13 @@ export default function MoviesDetailsPage() {
   const [error, setError] = useState(false);
   const [loading, setLoading] = useState(false);
   const location = useLocation();
+  const backLinkLocationRef = useRef(
+    location.state?.from || {
+      pathname: "/movies",
+      search: location.state?.search || "",
+    }
+  );
+
   useEffect(() => {
     const getMovieDetails = async () => {
       try {
@@ -21,7 +94,6 @@ export default function MoviesDetailsPage() {
         setLoading(true);
         const movieData = await fetchMovieDetails(movieId);
         setMovie(movieData);
-        setLoading(false);
       } catch (error) {
         console.log("Oops! Please try again!");
         setError(true);
@@ -32,6 +104,7 @@ export default function MoviesDetailsPage() {
 
     getMovieDetails();
   }, [movieId]);
+
   if (!movie) {
     return null;
   }
@@ -41,7 +114,7 @@ export default function MoviesDetailsPage() {
       {error && <p>Oops! Try again</p>}
       {loading && <p>Loading...</p>}
       <p>
-        <Link to={location.state}>Go back</Link>
+        <Link to={backLinkLocationRef.current}>Go back</Link>
       </p>
       <h1>{movie.title}</h1>
       <img
--- src/pages/MoviesPage/MoviesPage.jsx
+++ src/pages/MoviesPage/MoviesPage.jsx
@@ -1,12 +1,99 @@
+// import MovieList from "../../components/MovieList/MovieList";
+// import { searchMovies } from "../../tmdbApi";
+// import { useState } from "react";
+// import { useSearchParams } from "react-router-dom";
+
+// export default function MoviesPage() {
+//   const [movies, setMovies] = useState([]);
+//   const [error, setError] = useState(false);
+//   const [loading, setLoading] = useState(false);
+//   const [searchParams, setSearchParams] = useSearchParams();
+//   // const queryRef = useRef();
+
+//   const getApiMovie = async (query) => {
+//     try {
+//       setError(false);
+//       setLoading(true);
+//       const searchResults = await searchMovies(query);
+//       setMovies(searchResults);
+//       setLoading(false);
+//     } catch (error) {
+//       console.log("Oops! Please try again!");
+//       setError(true);
+//     } finally {
+//       setLoading(false);
+//     }
+//   };
+
+//   const queryFilter = searchParams.get("query") ?? "";
+
+//   const changeQueryFilter = (newFilter) => {
+//     searchParams.set("query", newFilter);
+//     setSearchParams(searchParams);
+//   };
+//   const handleSearch = () => {
+//     getApiMovie(queryFilter);
+//   };
+//   // const handleSearchSubmit = async (event) => {
+//   //   event.preventDefault();
+//   //   const query = queryRef.current.value;
+//   //   if (query.trim() === "") return;
+
+//   //   setSearchParams({ query });
+
+//   //   try {
+//   //     setError(false);
+//   //     setLoading(true);
+//   //     const moviesData = await searchMovies(query);
+//   //     setMovies(moviesData);
+//   //     setLoading(false);
+//   //   } catch (error) {
+//   //     console.log("Oops! Please try again!");
+//   //     setError(true);
+//   //   } finally {
+//   //     setLoading(false);
+//   //   }
+//   // };
+//   return (
+//     <div>
+//       <input
+//         type="text"
+//         value={queryFilter}
+//         autoComplete="off"
+//         placeholder="Search movie"
+//         autoFocus
+//         onChange={(e) => changeQueryFilter(e.target.value)}
+//       />
+//       <button onClick={handleSearch} type="submit">
+//         Search
+//       </button>
+//       {/* <form onSubmit={handleSearchSubmit}>
+//         <input
+//           type="text"
+//           ref={queryRef}
+//           placeholder="Search for movies..."
+//           autoComplete="off"
+//           autoFocus
+//         />
+//         <button type="submit">Search</button>
+//       </form> */}
+
+//       {error && <p>Oops! Try again</p>}
+//       {loading && <p>Loading...</p>}
+//       {movies.length > 0 && <MovieList gettedMovies={movies} />}
+//     </div>
+//   );
+// }
 import MovieList from "../../components/MovieList/MovieList";
 import { searchMovies } from "../../tmdbApi";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useSearchParams } from "react-router-dom";
 
 export default function MoviesPage() {
   const [movies, setMovies] = useState([]);
   const [error, setError] = useState(false);
   const [loading, setLoading] = useState(false);
+  const [searchParams, setSearchParams] = useSearchParams();
 
   const getApiMovie = async (query) => {
     try {
@@ -14,7 +101,6 @@ export default function MoviesPage() {
       setLoading(true);
       const searchResults = await searchMovies(query);
       setMovies(searchResults);
-      setLoading(false);
     } catch (error) {
       console.log("Oops! Please try again!");
       setError(true);
@@ -23,29 +109,35 @@ export default function MoviesPage() {
     }
   };
 
-  const [searchParams, setSearchParams] = useSearchParams();
   const queryFilter = searchParams.get("query") ?? "";
 
-  const changeQueryFilter = (newFilter) => {
-    searchParams.set("query", newFilter);
-    setSearchParams(searchParams);
+  useEffect(() => {
+    if (queryFilter) {
+      getApiMovie(queryFilter);
+    }
+  }, [queryFilter]);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const query = event.target.elements.query.value;
+    if (query) {
+      setSearchParams({ query });
+    }
   };
-  const handleSearch = () => {
-    getApiMovie(queryFilter);
-  };
+
   return (
     <div>
-      <input
-        type="text"
-        value={queryFilter}
-        autoComplete="off"
-        placeholder="Search movie"
-        autoFocus
-        onChange={(e) => changeQueryFilter(e.target.value)}
-      />
-      <button onClick={handleSearch} type="submit">
-        Search
-      </button>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="query"
+          defaultValue={queryFilter}
+          autoComplete="off"
+          placeholder="Search movie"
+          autoFocus
+        />
+        <button type="submit">Search</button>
+      </form>
       {error && <p>Oops! Try again</p>}
       {loading && <p>Loading...</p>}
       {movies.length > 0 && <MovieList gettedMovies={movies} />}
